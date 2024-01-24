a65275699192098d71957611f77f150d01155822
JJVenegas <147569743+JJVenegas@users.noreply.github.com>
2024-01-23 21:06:08 -0800

    finished

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ function newImage(url, left, bottom){
     return image
 }
 
+
+function move(){
+
+}
+
+
+
 newImage('assets/green-character.gif', 100, 250)
 newImage('assets/tree.png', 200, 450)
 newImage('assets/pillar.png', 350, 250)
